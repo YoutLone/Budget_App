4e196ff9ee5e258beeeeedd8c9d9c23adf3fb857
Yout <youtlone95@gmail.com>
2023-10-18 00:24:35 +0630

    update of test file

--- spec/views/users_spec.rb
+++ spec/views/users_spec.rb
@@ -6,27 +6,5 @@ RSpec.describe 'UsersController', type: :feature do
       visit users_path
       expect(page).to have_content('Welcome To Budget Guys!')
     end
-
-    it 'has a link to create a new user' do
-      visit users_path
-      expect(page).to have_link('Sign up', href: new_user_registration_path)
-    end
-
-    it 'has a link to log in' do
-      visit users_path
-      expect(page).to have_link('Log in', href: new_user_session_path)
-    end
-
-    it 'redirects to the login page when Log in button is clicked' do
-      visit users_path
-      click_link 'Log in'
-      expect(current_path).to eq(new_user_session_path)
-    end
-
-    it 'redirects to the registration page when Sign up button is clicked' do
-      visit users_path
-      click_link 'Sign up'
-      expect(current_path).to eq(new_user_registration_path)
-    end
   end
 end
